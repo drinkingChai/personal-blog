edb871771de59b79402ceac6eba8b40e96ce41f8
Wasif Zaman <teatocode@gmail.com>
2017-10-14 00:19:20 -0400

    all blogs route

--- server/api/blogs.js
+++ server/api/blogs.js
@@ -1,3 +1,10 @@
 const router = require('express').Router()
+const { Blog } = require('../db').models
+
+router.get('/', (req, res, next) => {
+  Blog.findAll({ order: [[ 'createdAt', 'DESC' ]] })
+    .then(blogs => res.send(blogs))
+    .catch(next)
+})
 
 module.exports = router
