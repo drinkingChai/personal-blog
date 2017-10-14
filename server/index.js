51b50f2293e7fcddac541ffe3100ef58355c54e5
Wasif Zaman <teatocode@gmail.com>
2017-10-14 10:49:50 -0400

    add client session and error handling

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const sessions = require('client-sessions')
 const db = require('./db')
 
 const app = express()
 app.use(require('morgan')('dev'))
+app.use(sessions({
+  cookieName: 'session',
+  secret: process.env.SESSIONSECRET,
+  maxAge: 30 * 60 * 1000
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -15,8 +21,11 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err)
+})
+
 const port = process.env.PORT || 3005
 
 db.sync()
   .then(() => app.listen(port, () => console.log(`listening on port ${port}`)))
-
